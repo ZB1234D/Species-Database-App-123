f8eb5df626e55474743feff92b994a53b49c5995
ZB1234D <s223465277@deakin.edu.au>
2026-02-06 23:35:30 +0300

    uploader

--- backend/app.py
+++ backend/app.py
@@ -280,12 +280,9 @@ def upload_species_file():
             temp_path = tmp.name
 
         en_result = asyncio.run(process_file(temp_path, translate=False))  # English
-        tet_result = asyncio.run(process_file(temp_path, translate=True))   # Tetum
+        tet_task = asyncio.create_task(process_file(temp_path, translate=True))   # Tetum
 
-        rows_inserted = min(
-            en_result["rows_inserted"],
-            tet_result["rows_inserted"]
-        )
+        rows_inserted = en_result["rows_inserted"],
         
         log_change(
             "species",
